254a83b9a4efd94c94f4809e3ea79909f46e317d
Rafaela Watanabe <rafaela.watanabee@gmail.com>
2024-09-30 22:07:10 -0300

    Initial commit for kinolens-frontend

---
+++ apps/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './features/authSlice';
+
+const rootReducer = {
+  auth: authReducer,
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
